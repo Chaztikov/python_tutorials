1fb798afee28df7f43d99adac8d231063448fdfa
chaztikov <chaztikov@gmail.com>
2019-06-11 23:55:00 -0400

    .

---
+++ scrapestocks/ex0/XDG_CACHE_HOME/Microsoft/Python Language Server/stubs.v1/ep2mNTrYdGXWfcQr4RuC1OKBXxAi4R1cYqWzN2U3vPs=/Lexicon.cpython-36m-x86_64-linux-gnu.pyi
@@ -0,0 +1,19 @@
+import __future__ as _mod___future__
+import builtins as _mod_builtins
+
+IDENT = 'IDENT'
+__builtins__ = {}
+__cached__ = '/usr/lib/python3/dist-packages/Cython/Compiler/__pycache__/Lexicon.cpython-37.pyc'
+__doc__ = None
+__file__ = '/usr/lib/python3/dist-packages/Cython/Compiler/Lexicon.py'
+__name__ = 'Cython.Compiler.Lexicon'
+__package__ = 'Cython.Compiler'
+absolute_import = _mod___future__._Feature()
+any_string_prefix = 'rRfFuUbBcC'
+bytes_prefixes = 'bB'
+char_prefixes = 'cC'
+def make_lexicon():
+    pass
+
+raw_prefixes = 'rR'
+string_prefixes = 'fFuUbB'
---
+++ scrapestocks/ex0/main.py
@@ -0,0 +1,37 @@
+import sys,os
+print(sys.version)
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime as dt
+
+style.use('fivethirtyeight')
+
+start=dt.datetime(2000,1,1)
+end=dt.datetime(2016,12,31)
+
+
+symbol = 'TSLA'
+server = 'yahoo'
+df=web.DataReader(symbol,server,start,end)
+
+# Rather than reading data from Yahoo's finance API to a DataFrame, we can also read data from a CSV file into a DataFrame:
+df.to_csv('TSLA.csv')
+# Now, we can graph with:
+df = pd.read_csv('TSLA.csv', parse_dates=True, index_col=0)
+
+
+print(df.head())
+print(df.label)
+print(df[['High','Low']])
+
+
+plt.figure()
+df['Adj Close'].plot() 
+df['High'].plot() 
+df['Low'].plot() 
+plt.legend()
+plt.show();
+
