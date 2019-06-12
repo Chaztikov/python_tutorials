f4dd5ece65f2d29387171063f6afd372b1658537
chaztikov <chaztikov@gmail.com>
2019-06-11 23:59:20 -0400

    .

--- scrapestocks/ex0/main.py
+++ scrapestocks/ex0/main.py
@@ -45,3 +45,15 @@ df['High'].plot()
 df['Low'].plot() 
 plt.legend()
 plt.show();
+
+
+
+# If you want to know more about subplot2grid, check out this subplots with Matplotlib tutorial.
+# Basically, we're saying we want to create two subplots, and both subplots are going to act like they're on a 6x1 grid, where we have 6 rows and 1 column. The first subplot starts at (0,0) on that grid, spans 5 rows, and spans 1 column. The next axis is also on a 6x1 grid, but it starts at (5,0), spans 1 row, and 1 column. The 2nd axis also has the sharex=ax1, which means that ax2 will always align its x axis with whatever ax1's is, and visa-versa. Now we just make our plots:
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1,sharex=ax1)
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+plt.show()
